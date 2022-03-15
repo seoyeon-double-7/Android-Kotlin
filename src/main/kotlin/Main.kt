f1188a51164412603ee3c8cdef09a5b0e0209a68
cindy0817-web <s2103@e-mirim.hs.kr>
2022-03-15 19:29:54 +0900

    변수 선언(var), 상수 선언(val)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-    var a= 100
-    println(a)
+
+//  var로 변수 선언하는데, 타입은 안써도 타입 추론을 해줌
+    var a: Int = 100
+    var s = "Hello"
+
+//   상수(val)
+    val PI = 3.14
 }
